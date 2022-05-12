184417bf5b70a716774cb3bbb58317a659456c56
gunessezgi <sezgi.gunes@noyirmibir.com>
2022-05-12 12:50:46 +0300

    first commit

---
+++ src/components/button/button.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./button.scss";
+
+export type ButtonProps = {
+  label: string
+}
+
+const Button = (props: ButtonProps) => {
+  return <button>{props.label}</button>;
+};
+
+export default Button;
